4d93dfeedd4b57dca5649b38734f493a964f6d7a
Lee-Git22 <lee.alan9801@gmail.com>
2022-11-22 14:38:31 -0500

    Implement getComputerChoice using rng

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Hello world");
+// Randomly selects a number between 1-3
+function getComputerChoice() {
+    var random = Math.floor(Math.random() * 3 + 1);
+    
+    // Declares computerSelection variable then assigns RPS value based on random number
+    let computerSelection = "";
+    if (random == 1) {
+        computerSelection = "Rock";
+    }
+    else if (random == 2) {
+        computerSelection = "Paper";
+    }
+    else if (random == 3) {
+        computerSelection = "Scissors";
+    }
+
+    console.log(computerSelection);
+    return computerSelection;
+}
+
+// TODO:
+function playRound(playerSelection, computerSelection) {
+    return;
+ }
+
+
+getComputerChoice();
+
